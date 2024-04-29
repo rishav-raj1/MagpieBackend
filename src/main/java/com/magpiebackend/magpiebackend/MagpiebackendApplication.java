24a361ef2a413b3e3e587f58a902d3d8d834e5d3
Rishav Kumar <81378195+rishav-raj1@users.noreply.github.com>
2024-04-29 09:10:24 +0530

    first change

---
+++ src/main/java/com/magpiebackend/magpiebackend/MagpiebackendApplication.java
@@ -0,0 +1,13 @@
+package com.magpiebackend.magpiebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagpiebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagpiebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magpiebackend/magpiebackend/MagpiebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magpiebackend.magpiebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagpiebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
