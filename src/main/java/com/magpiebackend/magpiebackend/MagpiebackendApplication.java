c0019c7ddd92af6299c4460e8b022b22a468822c
Rishav Kumar <81378195+rishav-raj1@users.noreply.github.com>
2024-05-22 16:28:09 +0530

    First Get Api

--- src/main/java/com/magpiebackend/magpiebackend/MagpiebackendApplication.java
+++ src/main/java/com/magpiebackend/magpiebackend/MagpiebackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MagpiebackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MagpiebackendApplication.class, args);
 	}
 
---
+++ src/main/java/com/magpiebackend/magpiebackend/controller/CloudVendorAPIService.java
@@ -0,0 +1,17 @@
+package com.magpiebackend.magpiebackend.controller;
+
+import com.magpiebackend.magpiebackend.Modal.CloudVendor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService
+{
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId)
+    {
+        return new CloudVendor("C1", "Vendor 1", "Address One", "xxxxx");
+    }
+}
