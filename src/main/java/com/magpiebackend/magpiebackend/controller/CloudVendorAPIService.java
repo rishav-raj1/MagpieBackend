e23f4d6fa7e0c3aaa416145b787982c8ed0b4761
Rishav Kumar <81378195+rishav-raj1@users.noreply.github.com>
2024-05-22 16:45:40 +0530

    Second Post Api

--- src/main/java/com/magpiebackend/magpiebackend/controller/CloudVendorAPIService.java
+++ src/main/java/com/magpiebackend/magpiebackend/controller/CloudVendorAPIService.java
@@ -1,18 +1,24 @@
 package com.magpiebackend.magpiebackend.controller;
 
 import com.magpiebackend.magpiebackend.Modal.CloudVendor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cloudvendor")
 public class CloudVendorAPIService
 {
+    CloudVendor cloudVendor;
     @GetMapping("{vendorId}")
     public CloudVendor getCloudVendorDetails(String vendorId)
     {
-        return new CloudVendor("C1", "Vendor 1", "Address One", "xxxxx");
+        return cloudVendor;
+//                new CloudVendor("C1", "Vendor 1", "Address One", "xxxxx");
+    }
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+       this.cloudVendor = cloudVendor;
+       return "Cloud Vendor Created Successfully";
     }
 
 }
